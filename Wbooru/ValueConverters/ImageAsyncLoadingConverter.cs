85bfd1c5bc117dc9bd0e7000c40ec73dc201c3c1
Mikira Sora <mikirasora0409@126.com>
2019-05-19 00:01:39 +0800

    fix lag when a lot image need convert to bitmapimage

--- Wbooru/ValueConverters/ImageAsyncLoadingConverter.cs
+++ Wbooru/ValueConverters/ImageAsyncLoadingConverter.cs
@@ -21,25 +21,22 @@ namespace Wbooru.ValueConverters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return new AsyncTask(() => {
+            return new AsyncImageWrapper(async () =>
+            {
                 var downloader = Container.Default.GetExportedValue<ImageFetchDownloadSchedule>();
-                var image = downloader.GetImageAsync(value.ToString()).Result;
+                var image = await downloader.GetImageAsync(value.ToString());
 
                 using var stream = new MemoryStream();
                 image.Save(stream, image.RawFormat);
                 stream.Seek(0, SeekOrigin.Begin);
 
-                BitmapImage source=null;
-
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    source=new BitmapImage();
-                    source.BeginInit();
-                    source.StreamSource = stream;
-                    source.CacheOption = BitmapCacheOption.OnLoad;
-                    source.EndInit();
-                    source.Freeze();
-                });
+                BitmapImage source = null;
+                source = new BitmapImage();
+                source.BeginInit();
+                source.StreamSource = stream;
+                source.CacheOption = BitmapCacheOption.OnLoad;
+                source.EndInit();
+                source.Freeze();
 
                 return source;
             });
@@ -50,28 +47,29 @@ namespace Wbooru.ValueConverters
             throw new NotImplementedException();
         }
 
-        public class AsyncTask : INotifyPropertyChanged
+        public class AsyncImageWrapper : DependencyObject
         {
-            public AsyncTask(Func<BitmapImage> valueFunc)
+            public AsyncImageWrapper(Func<Task<BitmapImage>> valueFunc)
             {
-                AsyncValue = default(BitmapImage);
                 LoadValue(valueFunc);
             }
 
-            private async void LoadValue(Func<BitmapImage> valueFunc)
+            private async void LoadValue(Func<Task<BitmapImage>> valueFunc)
             {
                 AsyncValue = await Task.Run(() =>
                 {
                     return valueFunc();
                 });
-
-                if (PropertyChanged != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs("AsyncValue"));
             }
 
-            public BitmapImage AsyncValue { get; set; }
+            public BitmapImage AsyncValue
+            {       
+                get { return (BitmapImage)GetValue(AsyncValueProperty); }
+                set { SetValue(AsyncValueProperty, value); }
+            }
 
-            public event PropertyChangedEventHandler PropertyChanged;
+            public static readonly DependencyProperty AsyncValueProperty =
+                DependencyProperty.Register("AsyncValue", typeof(BitmapImage), typeof(AsyncImageWrapper), new PropertyMetadata(default(BitmapImage)));
         }
     }
 }
