35ef43f43b3e4cb02b4e7ffeb2e78efb17dc0c38
Mikira Sora <mikirasora0409@126.com>
2019-05-29 22:27:30 +0800

    finish LoadingTaskNotify

--- Wbooru/UI/Controls/LoadingStatusDisplayer.xaml.cs
+++ Wbooru/UI/Controls/LoadingStatusDisplayer.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,11 +22,38 @@ namespace Wbooru.UI.Controls
         public LoadingStatusDisplayer()
         {
             InitializeComponent();
+            trans = Resources["Trans"] as Storyboard;
 
-            DataContext = this;
+            Loaded += (a, b) => Active();
+
+            MainPanel.DataContext = this;
+            show_action = Resources["ShowAction"] as Storyboard;
+            hide_action = Resources["HideAction"] as Storyboard;
         }
 
         HashSet<LoadingTaskNotify> reg_notifies = new HashSet<LoadingTaskNotify>();
+        private Storyboard show_action;
+        private Storyboard hide_action;
+        private readonly Storyboard trans;
+
+        public string Description
+        {
+            get { return (string)GetValue(DescriptionProperty); }
+            set { SetValue(DescriptionProperty, value); }
+        }
+
+        public string TaskCount
+        {
+            get { return (string)GetValue(TaskCountProperty); }
+            set { SetValue(TaskCountProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for TaskCount.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TaskCountProperty =
+            DependencyProperty.Register("TaskCount", typeof(string), typeof(LoadingStatusDisplayer), new PropertyMetadata(""));
+
+        public static readonly DependencyProperty DescriptionProperty =
+            DependencyProperty.Register("Description", typeof(string), typeof(LoadingStatusDisplayer), new PropertyMetadata(""));
 
         public LoadingTaskNotify BeginBusy(string description = null)
         {
@@ -36,13 +64,45 @@ namespace Wbooru.UI.Controls
 
             reg_notifies.Add(t);
 
+            Description = reg_notifies.Last().Description;
+
+            show_action.Begin();
+            UpdateCountString();
             return t;
+        }
+
+        private void UpdateCountString()
+        {
+            TaskCount = reg_notifies.Count <= 1 ? "" : (reg_notifies.Count - 1).ToString();
         }
 
         internal void TaskFinishNotify(LoadingTaskNotify t)
         {
             reg_notifies.Remove(t);
             ObjectPool<LoadingTaskNotify>.Return(t);
+
+            if (reg_notifies.Count==0)
+            {
+                hide_action.Begin();
+            }
+
+            Description = reg_notifies.LastOrDefault()?.Description ?? Description;
+            UpdateCountString();
+        }
+
+        private async void Active()
+        {
+            el.BeginStoryboard(trans);
+            await Task.Delay(170);
+            el2.BeginStoryboard(trans);
+            await Task.Delay(170);
+            el3.BeginStoryboard(trans);
+            await Task.Delay(170);
+            el4.BeginStoryboard(trans);
+            await Task.Delay(170);
+            el5.BeginStoryboard(trans);
+            await Task.Delay(170);
+            el6.BeginStoryboard(trans);
         }
 
         public bool IsBusy => reg_notifies.Count!=0;
