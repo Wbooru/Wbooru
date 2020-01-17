647efc2a8b9e489027d3a3649679911f2fe12bfd
Mikira Sora <mikirasora0409@126.com>
2020-01-18 00:32:22 +0800

    add PreApplyDBFileIfNotExist

--- Wbooru/Persistence/DBConnectionFactory.cs
+++ Wbooru/Persistence/DBConnectionFactory.cs
@@ -27,6 +27,8 @@ namespace Wbooru.Persistence
                 return _cached_connection;
 
 #if SQLITE_DEBUG || SQLITE
+            PreApplyDBFileIfNotExist();
+
             var db_file_path = Path.GetFullPath(SettingManager.LoadSetting<GlobalSetting>().DBFilePath);
             Log.Info($"Use sqlite db , db file :{db_file_path}");
 
@@ -42,6 +44,23 @@ namespace Wbooru.Persistence
             Log.Info($"Use localdb");
             return _cached_connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
 #endif
+        }
+
+        private static void PreApplyDBFileIfNotExist()
+        {
+            var path = Path.GetFullPath(SettingManager.LoadSetting<GlobalSetting>().DBFilePath);
+
+            if (File.Exists(path))
+                return;
+
+            //try get default empty database file
+            var default_db_path = "default_empty.db";
+
+            if (!File.Exists(default_db_path))
+                throw new Exception("Wbooru没有数据库，也没找到default_empty.db以及其他的数据库文件资源，请自行生成数据库文件.");
+
+            Log.Info($"user database file not found , move default database file {default_db_path} to {path}");
+            File.Copy(default_db_path, path);
         }
     }
 }
