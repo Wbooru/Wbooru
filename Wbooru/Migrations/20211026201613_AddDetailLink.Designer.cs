e86c7fac695ffac75400352126d7df8c67a3155c
MikiraSora <mikirasora0409@126.com>
2021-10-27 04:17:41 +0800

    add migration AddDetailLink

--- Wbooru/Migrations/20211026201613_AddDetailLink.Designer.cs
+++ Wbooru/Migrations/20211026201613_AddDetailLink.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Wbooru.Persistence;
 
 namespace Wbooru.Migrations
 {
     [DbContext(typeof(LocalDBContext))]
-    partial class LocalDBContextModelSnapshot : ModelSnapshot
+    [Migration("20211026201613_AddDetailLink")]
+    partial class AddDetailLink
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Wbooru/Migrations/20211026201613_AddDetailLink.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wbooru.Migrations
+{
+    public partial class AddDetailLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DetailLink",
+                table: "GalleryItems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DetailLink",
+                table: "GalleryItems");
+        }
+    }
+}
