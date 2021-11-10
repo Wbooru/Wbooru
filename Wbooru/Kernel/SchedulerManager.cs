3dc2a38ff190c171f72c1d67d16661e27eddf0ce
MikiraSora <mikirasora0409@126.com>
2021-11-11 04:02:17 +0800

    refactory SchedulerManager

--- Wbooru/Kernel/SchedulerManager.cs
+++ Wbooru/Kernel/SchedulerManager.cs
@@ -6,75 +6,51 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Wbooru.PluginExt;
+using Wbooru.Utils;
 
 namespace Wbooru.Kernel
 {
     public static class SchedulerManager
     {
+        private static AbortableThread runThread;
+
         private static List<ISchedulable> schedulers { get; } = new List<ISchedulable>();
 
-        private static Dictionary<ISchedulable, bool> schedulersAsyncLocker { get; } = new();
         private static Dictionary<ISchedulable, DateTime> schedulersCallTime { get; } = new();
 
         public static IEnumerable<ISchedulable> Schedulers => schedulers;
 
-        private static CancellationTokenSource schedule_thread_cancel_token = new CancellationTokenSource();
-
         public static void Init()
         {
-            schedule_thread_cancel_token = new CancellationTokenSource();
-            Task.Run(Run, schedule_thread_cancel_token.Token);
-
             foreach (var s in Container.Default.GetExportedValues<ISchedulable>())
-            {
                 AddScheduler(s);
-            }
+
+            runThread = new AbortableThread(Run);
+            runThread.Start();
         }
 
         public static void AddScheduler(ISchedulable s)
         {
-            if (s is null || schedulers.Contains(s))
+            if (s is null || schedulers.FirstOrDefault(x => x.SchedulerName.Equals(s.SchedulerName)) != null)
+            {
+                Log.Warn($"Can't add scheduler : {s?.SchedulerName} is null/exist.");
                 return;
+            }
 
             schedulers.Add(s);
-            schedulersAsyncLocker[s] = false;
             schedulersCallTime[s] = DateTime.MinValue;
             Log.Info("Added new scheduler: " + s.SchedulerName);
         }
 
-        private static async void Run()
+        private static async void Run(CancellationToken cancellationToken)
         {
-            while (!schedule_thread_cancel_token.IsCancellationRequested)
+            while (!cancellationToken.IsCancellationRequested)
             {
-                for (int i = 0; i < Schedulers.Count(); i++)
-                {
-                    var schedule = Schedulers.ElementAt(i);
-
-                    if (DateTime.Now - schedulersCallTime[schedule] < schedule.ScheduleCallLoopInterval)
-                        continue;
-
-                    if (schedule.IsAsyncSchedule)
-                    {
-                        if (schedulersAsyncLocker[schedule])
-                            continue;
-                        schedulersAsyncLocker[schedule] = true;
-
-#pragma warning disable CS4014 // 由于此调用不会等待，因此在调用完成前将继续执行当前方法
-                        Task.Run(async () =>
-                        {
-                            await schedule.OnScheduleCall(schedule_thread_cancel_token.Token);
-                            schedulersAsyncLocker[schedule] = false;
-                            schedulersCallTime[schedule] = DateTime.Now;
-                        });
-#pragma warning restore CS4014 // 由于此调用不会等待，因此在调用完成前将继续执行当前方法
-                    }
-                    else
-                    {
-                        await schedule.OnScheduleCall(schedule_thread_cancel_token.Token);
-                        schedulersCallTime[schedule] = DateTime.Now;
-                    }
-                }
-
+                var schedulers = Schedulers
+                    .Where(x => DateTime.Now - schedulersCallTime[x] >= x.ScheduleCallLoopInterval)
+                    .Select(x => x.OnScheduleCall(cancellationToken).ContinueWith(_ => schedulersCallTime[x] = DateTime.Now))
+                    .ToArray();
+                await Task.WhenAll(schedulers);
                 await Task.Yield();
             }
         }
@@ -83,7 +59,7 @@ namespace Wbooru.Kernel
         {
             try
             {
-                schedule_thread_cancel_token.Cancel();
+                runThread.Abort();
             }
             catch { }
 
