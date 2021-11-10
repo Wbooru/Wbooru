6c3322ca1dfd97465b11748c6e533a1f944a8815
MikiraSora <mikirasora0409@126.com>
2021-11-11 03:39:57 +0800

    impl AbortableThread for safe

---
+++ Wbooru/Utils/AbortableThread.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Wbooru.Utils
+{
+    public class AbortableThread
+    {
+        private Thread thread;
+        private CancellationTokenSource cancellationTokenSource;
+
+        public AbortableThread(Action<CancellationToken> cancellableMethod)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            thread = new Thread(() => cancellableMethod?.Invoke(cancellationTokenSource.Token));
+        }
+
+        public bool IsBackground
+        {
+            get
+            {
+                return thread.IsBackground;
+            }
+
+            set
+            {
+                thread.IsBackground = value;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return thread.Name;
+            }
+            set
+            {
+                thread.Name = value;
+            }
+        }
+
+        public void Start()
+        {
+            thread.Start();
+        }
+
+
+        public void Abort()
+        {
+            cancellationTokenSource.Cancel();
+            thread?.Join();
+        }
+    }
+}
